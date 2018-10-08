d007bd7b63489786bbcfba3d5b297d403a0e30e0
Brianna O'Keefe <bri@songoda.com>
2018-10-08 15:27:18 -0400

    Fixed near not getting the entitys

--- EpicSpawners-Plugin/src/main/java/com/songoda/epicspawners/handlers/AppearanceHandler.java
+++ EpicSpawners-Plugin/src/main/java/com/songoda/epicspawners/handlers/AppearanceHandler.java
@@ -127,7 +127,8 @@ public class AppearanceHandler {
         Location location = spawner.getLocation();
         location.add(.5, -.4, .5);
 
-        List<Entity> near = (List<Entity>) location.getWorld().getNearbyEntities(location, 1, 2, 1);
+        if (near == null)return null;
+        List<Entity> near = (List<Entity>) location.getWorld().getNearbyEntities(location, 2, 4, 2);
         near.removeIf(e -> (!(e.getCustomName().equalsIgnoreCase("EpicSpawners-Display"))));
         if (near.size() != 0) {
             if(Debugger.isDebug()){
